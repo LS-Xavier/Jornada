f8ffca797cb897caa178bf9db1dc518cf5db34b8
LS-Xavier <leo.sant4@gmail.com>
2020-09-20 10:49:53 -0300

    organizando arquivos

--- Controllers/Atendimento.js
+++
@@ -1,9 +0,0 @@
-//funçaõ: determinar as rotas 
-
-module.exports = app => {
-    app.get("/atendimentos", (req, res)=> res.send('rota de atendimentos atualizado com nodemon'))
-
-    app.post("/atendimento", (req, res) => {
-        console.log(req.body)
-        res.send("postando dados para o servidor")})
-}
